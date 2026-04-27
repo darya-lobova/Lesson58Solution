e63dbcb83a8224c615ff5e5a102ed71f83f68218
st <st@A510PC3>
2026-04-27 17:49:34 +0300

    test06 update

---
+++ Task01/test06.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+// + - ++ --
+int main() {
+	short number = 10;
+	short* ptr1 = NULL;
+	short* ptr2 = NULL;
+	ptr1 = &number;
+	ptr2 = ptr1 + 10;
+
+
+	cout << "Result: " << (ptr2 - ptr1) << endl;
+	
+
+	return 0;
+}
