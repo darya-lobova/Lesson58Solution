0249a0bf2dd220f78262b14b741e7f9144f6da63
st <st@A510PC3>
2026-04-27 17:36:28 +0300

    test04 update

--- Task01/test04.cpp
+++ Task01/test04.cpp
@@ -1,25 +1,20 @@
-////pointer
-//#include <iostream>
-//using namespace std;
-//
-//int main() {
-//	int* ptr_a = nullptr;
-//	short* ptr_h = NULL;
-//	float* ptr_f = nullptr;
-//	double* ptr_d = NULL;
-//	long long* ptr_l = nullptr;
-//	char* ptr_c = NULL;
-//	bool* ptr_b = nullptr;
-//	void* ptr_v = NULL;
-//
-//	cout << "Value: " << ptr_a << endl;
-//	cout << "Value: " << ptr_d << endl;
-//	cout << "Value: " << ptr_f << endl;
-//	//cout << "Value: " << ptr_c << endl;
-//	cout << "Value: " << ptr_l << endl;
-//	cout << "Value: " << ptr_h << endl;
-//	cout << "Value: " << ptr_b << endl;
-//	cout << "Value: " << ptr_v << endl;
-//
-//	return 0;
-//}
+#include <iostream>
+using namespace std;
+
+// &
+// *
+
+int main() {
+	int* ptr = NULL;
+
+	int number = 10;
+	ptr = &number;
+
+	cout << "Before: number = " << number << endl;
+
+	*ptr = *ptr + 5;
+
+	cout << "After: number = " << number << endl;
+	
+	return 0;
+}
