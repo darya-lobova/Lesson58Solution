3cb6569ba37b20fdeec07928ce05a335197adef3
st <st@A510PC3>
2026-04-27 17:43:32 +0300

    test05 update

--- Task01/test04.cpp
+++ Task01/test04.cpp
@@ -1,20 +1,20 @@
-#include <iostream>
-using namespace std;
-
-// &
-// *
-
-int main() {
-	int* ptr = NULL;
-
-	int number = 10;
-	ptr = &number;
-
-	cout << "Before: number = " << number << endl;
-
-	*ptr = *ptr + 5;
-
-	cout << "After: number = " << number << endl;
-	
-	return 0;
-}
+//#include <iostream>
+//using namespace std;
+//
+//// &
+//// *
+//
+//int main() {
+//	int* ptr = NULL;
+//
+//	int number = 10;
+//	ptr = &number;
+//
+//	cout << "Before: number = " << number << endl;
+//
+//	*ptr = *ptr + 5;
+//
+//	cout << "After: number = " << number << endl;
+//	
+//	return 0;
+//}
---
+++ Task01/test05.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+// + - ++ --
+int main() {
+	int number = 10;
+	int* ptr = NULL;
+	ptr = &number;
+
+	cout << "0)" << ptr << endl;
+	cout << "1)" << ++ptr << endl;
+	cout << "2)" << ++ptr << endl;
+	cout << "3)" << ++ptr << endl;
+	cout << "4)" << ++ptr << endl;
+
+	return 0;
+}
